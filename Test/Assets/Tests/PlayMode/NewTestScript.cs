4094ddb9474639e30e1fb992819733ba040baf17
Putra Alrasy <putra.alrasy@generalarcade.com>
2023-10-30 12:33:56 +0800

    COmmit

--- Test/Assets/Tests/PlayMode/NewTestScript.cs
+++ Test/Assets/Tests/PlayMode/NewTestScript.cs
@@ -11,6 +11,11 @@ namespace Tests
             count = 0;
         }
 
+        public BasicCounter(int initialCount) // Constructor with initial count
+        {
+            count = initialCount;
+        }
+
         public int Count
         {
             get { return count; }
@@ -35,6 +40,15 @@ namespace Tests
 
             // Assert
             Assert.AreEqual(1, counter.Count);
+        }
+
+        [Test]
+        public void TestMaxCount()
+        {
+            var counter = new BasicCounter(10); // Create with an initial count of 10
+
+            counter.Increment();
+            Assert.AreEqual(11, counter.Count); // Expect the count to be 11 after increment
         }
     }
 }
